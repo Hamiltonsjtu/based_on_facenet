9b4a9694b81a4cca27e1bab9eebd1110fcce82a8
Hamiltonsjtu <zhushuaisjtu@qq.com>
2019-06-06 10:21:07 +0800

    Use the pre-trained model

--- serving/img_client.py
+++ serving/img_client.py
@@ -2,3 +2,4 @@ import requests
 # #######-------------image client------------#######
 files_ ={"file": open("e:/shuai/Face/hu_ying.jpg" ,"rb")}
 r = requests.post("http://127.0.0.1:5000/upload", files=files_)
+print(r.text)
