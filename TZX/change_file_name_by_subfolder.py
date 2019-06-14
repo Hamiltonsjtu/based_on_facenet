9b326df9d9f1e71a697516eb51159f7a62b87b4d
Hamiltonsjtu <zhushuaisjtu@qq.com>
2019-06-14 17:28:36 +0800

    alignment_percentage_margin_multiple_faces

--- TZX/change_file_name_by_subfolder.py
+++ TZX/change_file_name_by_subfolder.py
@@ -29,10 +29,10 @@ def main(args):
         if os.path.isdir(pic_folder_dir):
             folder_name = pic_folder
             pic_folder_dir_pics = os.listdir(pic_folder_dir)
-            # print('pic_folder is {} and is directionary is {}'.format(folder_name, pic_folder_dir_pics))
-            # print('==========================================')
+
             for i, file_name in enumerate(pic_folder_dir_pics):
-                rename_file = folder_name + '_' + '{:04d}'.format(i) + '.png'
+                filename_base, file_extension = os.path.splitext(file_name)
+                rename_file = folder_name + '_' + '{:04d}'.format(i) + file_extension
                 src_ = os.path.join(pic_folder_dir, file_name)
                 dst_ = os.path.join(pic_folder_dir, rename_file)
                 if os.path.isfile(dst_):
