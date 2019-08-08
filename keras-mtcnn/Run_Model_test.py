3ffeec9b590d8ed5d3f9af0dedda52e020275703
Hamiltonsjtu <zhushuaisjtu@qq.com>
2019-08-08 18:50:57 +0800

    mtcnn_keras_pack

--- keras-mtcnn/Run_Model_test.py
+++ keras-mtcnn/Run_Model_test.py
@@ -10,9 +10,7 @@ from keras.models import Model, Sequential
 import tensorflow as tf
 from keras.layers.advanced_activations import PReLU
 from keras.layers import Layer, Concatenate, Reshape
-
-
-def _Onet( weight_path = 'model48.h5'):
+def _Onet(weight_path = 'model48.h5'):
     input = Input(shape = [48,48,3])
     x = Conv2D(32, (3, 3), strides=1, padding='valid', name='conv1')(input)
     x = PReLU(shared_axes=[1,2],name='prelu1')(x)
@@ -37,9 +35,7 @@ def _Onet( weight_path = 'model48.h5'):
     model.load_weights(weight_path, by_name=True)
 
     return model
-
-
-def _Rnet (weight_path = 'model24.h5'):
+def _Rnet(weight_path = 'model24.h5'):
     input = Input(shape=[24, 24, 3])  # change this shape to [None,None,3] to enable arbitraty shape input
     x = Conv2D(28, (3, 3), strides=1, padding='valid', name='conv1')(input)
     x = PReLU(shared_axes=[1, 2], name='prelu1')(x)
@@ -60,45 +56,92 @@ def _Rnet (weight_path = 'model24.h5'):
     model = Model([input], [classifier, bbox_regress])
     model.load_weights(weight_path, by_name=True)
     return model
-
-class _PNET(Layer):
+class _PNET_post(Layer):
     def __init__(self, **kwargs):
-        super(_PNET, self).__init__(**kwargs)
+        super(_PNET_post, self).__init__(**kwargs)
         self.scales     = 1.0
         self.threshold  = 0.6
-
-    def tffix(self, tensor):
-        mask1 = tensor >= 0
-        tensor = tf.where(mask1, lambda: tf.floor(tensor), lambda : tf.ceil(tensor))
-        return tensor
-    def call(self, inputs):
-        classifier = inputs[0]
-        bbox_regress = inputs[1]
-        Cls_pro = tf.transpose(tf.slice(classifier, [0,0,0,1], [-1,-1,-1,1]), perm=[0, 2, 1, 3])
-        Roi = tf.transpose(tf.slice(bbox_regress, [0,0,0,0], [-1,-1,-1,-1]), perm=[0, 3, 2, 1])
+    def tffix(self, mat):
+        mask1 = mat >= 0
+        a_floor = tf.math.floor(mat)
+        b_ceil = tf.math.ceil(mat)
+        remat = tf.where(mask1, a_floor, b_ceil)
+        return tf.cast(remat, dtype=tf.float32)
+    def rec2square(self, rectangles):
+        w = tf.subtract(tf.gather(rectangles, 2, axis=-1), tf.gather(rectangles, 0, axis=-1))
+        h = tf.subtract(tf.gather(rectangles, 3, axis=-1), tf.gather(rectangles, 1, axis=-1))
+        l = tf.maximum(w, h)
+        rec_1 = tf.gather(rectangles,0, axis=-1) + w*0.5 - l*0.5
+        rec_2 = tf.gather(rectangles,1, axis=-1) + h*0.5 - l*0.5
+        rec_3 = rec_1 + l
+        rec_4 = rec_2 + l
+        rec_5 = tf.gather(rectangles,4, axis=-1)
+        rectangles = tf.transpose(tf.concat([[rec_1], [rec_2], [rec_3], [rec_4], [rec_5]], axis=0), perm=[1,0])
+        return rectangles
+    def cls_bb2rect(self, Cls_pro, Roi):
         scale = self.scales
-        width = tf.shape(Cls_pro)[1]
-        height = tf.shape(Cls_pro)[2]
+        width = tf.shape(Cls_pro)[0]
+        height = tf.shape(Cls_pro)[1]
         out_side = tf.maximum(width, height)
-        in_side = 2*out_side + 11
-        cond = tf.not_equal(2*out_side, 11)
-        ftrue = tf.cast((in_side - 12)/(out_side -1), dtype=tf.float32)
+        in_side = 2 * out_side + 11
+        cond = tf.not_equal(2 * out_side, 11)
+        ftrue = tf.cast((in_side - 12) / (out_side - 1), dtype=tf.float32)
         ffals = tf.constant(0.0, dtype=tf.float32)
         stride = tf.cond(cond, lambda: ftrue, lambda: ffals)
         threshold_p = self.threshold
-
-        thresh_index = tf.where(Cls_pro >= threshold_p)
-        boundingbox = tf.expand_dims(tf.slice(thresh_index, [0, 1], [-1, 2]), axis=0)
-        bb1_tmp = tf.math.scalar_mul(stride, tf.cast(boundingbox, dtype=tf.float32))
-        bb2_tmp = tf.math.scalar_mul(stride, tf.cast(boundingbox, dtype=tf.float32))
+        Cls_pro_coor = tf.gather(Cls_pro, 0, axis=2)
+        thresh_index = tf.where(Cls_pro_coor >= threshold_p)
+        boundingbox = tf.cast(tf.slice(thresh_index, [0, 0], [-1, 2]), dtype=tf.float32)
+        bb1_tmp = tf.math.scalar_mul(stride, boundingbox)
+        bb2_tmp = tf.math.scalar_mul(stride, boundingbox) + 11.0
         bb1 = self.tffix(bb1_tmp)
         bb2 = self.tffix(bb2_tmp)
-        return boundingbox
+        boundingbox = tf.concat([bb1, bb2], -1)
+        dx1 = tf.squeeze(tf.gather_nd(tf.gather(Roi, 0), [thresh_index]))
+        dx2 = tf.squeeze(tf.gather_nd(tf.gather(Roi, 1), [thresh_index]))
+        dx3 = tf.squeeze(tf.gather_nd(tf.gather(Roi, 2), [thresh_index]))
+        dx4 = tf.squeeze(tf.gather_nd(tf.gather(Roi, 3), [thresh_index]))
+        compare = tf.gather(Roi, 0)
+        Cls_pro_GA = tf.gather(Cls_pro, [0], axis=2)
+        score = tf.expand_dims(tf.squeeze(tf.gather_nd(Cls_pro_GA, [thresh_index])), axis=1)
+        offset = tf.squeeze(tf.stack([dx1, dx2, dx3, dx4], axis=-1))
+        boundingbox = tf.squeeze(boundingbox + offset * 12.0 * scale)
+        rectangles = tf.concat([boundingbox, score], axis=-1)
+        rectangles = self.rec2square(rectangles)
+        return rectangles
+    def post_data_Pnet(self, classifier, bbox_regress, img_shape):
+        Cls_pro = tf.transpose(tf.gather(classifier, [1], axis=2), perm=[1, 0, 2])
+        Roi = tf.transpose(tf.slice(bbox_regress, [0,0,0], [-1,-1,-1]), perm=[2, 1, 0])
+        rectangles = self.cls_bb2rect(Cls_pro, Roi)
+        img_shape = img_shape
+        # pick = []
+        # for i in range(tf.shape[1]):
+        #     x1 = tf.cast(tf.maximum(0, rectangles[0][i][0]), dtype=tf.int32)
+        #     y1 = tf.cast(tf.maximum(0, rectangles[0][i][1]), dtype=tf.int32)
+        #     x2 = tf.cast(tf.minimum(width, rectangles[0][i][2]), dtype=tf.int32)
+        #     y2 = tf.cast(tf.minimum(height, rectangles[0][i][3]), dtype=tf.int32)
+        #     sc = rectangles[0][i][4]
+        #
+        #     if x2>x1 and y2>y1:
+        #         pick = Concatenate(axis=-2)(pred_masks)
+        return rectangles, rectangles, rectangles
 
+    def call(self, inputs):
+        classifier = inputs[0]
+        bbox_regress = inputs[1]
+        img_shape = inputs[2]
+        batch_data = (classifier, bbox_regress, img_shape)
+        bb1, bb2, bb3 = tf.map_fn(lambda x: self.post_data_Pnet(x[0], x[1], x[2]), batch_data, dtype=(tf.float32, tf.float32, tf.float32))
+        return [bb1, bb2, bb3]
+
+    # def compute_output_shape(self, input_shape):
+    #     shape = tf.TensorShape(input_shape).as_list()
+    #     return tf.TensorShape(shape)
 
 def _Pnet(weight_path = 'model12.h5'):
-    input = Input(shape=[None, None, 3])
-    x = Conv2D(10, (3, 3), strides=1, padding='valid', name='conv1')(input)
+    input_img = Input(shape=[None, None, 3])
+    input_shape = Input(shape=(2,))
+    x = Conv2D(10, (3, 3), strides=1, padding='valid', name='conv1')(input_img)
     x = PReLU(shared_axes=[1,2],name='PReLU1')(x)
     x = MaxPool2D(pool_size=2)(x)
     x = Conv2D(16, (3, 3), strides=1, padding='valid', name='conv2')(x)
@@ -109,45 +152,14 @@ def _Pnet(weight_path = 'model12.h5'):
     bbox_regress = Conv2D(4, (1, 1), name='conv4-2')(x)
 
     ### ----- ADD LAYER  ----  ###
-    outs = _PNET(name='Pnet_outs')([classifier, bbox_regress])
-    model = Model(inputs=[input], outputs=[outs])
-
+    outs = _PNET_post(name='Pnet_output_post')([classifier, bbox_regress, input_shape])
+    model = Model(inputs = [input_img, input_shape], outputs = outs)
     ###  LOAD TRAINED WEIGHTS
     model.load_weights(weight_path, by_name=True)
     return model
-
-
 Pnet = _Pnet(r'12net.h5')
-Rnet = _Rnet(r'24net.h5')
-Onet = _Onet(r'48net.h5')
-
-
-threshold = [0.6,0.6,0.7]
-# video_path = 'WalmartArguments_p1.mkv'
-# cap = cv2.VideoCapture(video_path)
-
-
-def resize_img(x):
-    origin_h = tf.cast(tf.shape(x)[1], dtype=tf.float32, name='height')
-    origin_w = tf.cast(tf.shape(x)[2], dtype=tf.float32, name='width')
-    hs = tf.cast(tf.multiply(origin_h, tf.constant(3.0)), dtype=tf.int32)
-    ws = tf.cast(tf.multiply(origin_w, tf.constant(3.0)), dtype=tf.int32)
-    output = tf.image.resize_images(x, [ws, hs],
-                  align_corners=False,
-                  preserve_aspect_ratio=False,
-                  name='scaled_img')
-    return output
-
-
-def Pnet_out(x):
-    bbox = x[0]
-    pro = x[1]
-    cls_pro = tf.gather(tf.slice(pro, [0,0,0,1], [1,-1,-1,1]), [0])
-    roi = tf.gather(tf.slice(bbox, [0,0,0,0], [1,-1,-1,-1]), [0])
-
-    return cls_pro, roi
-
-
+# Rnet = _Rnet(r'24net.h5')
+# Onet = _Onet(r'48net.h5')
 def get_layer_output(img, model, layer_name):
     layer = model.get_layer(name=layer_name)
     # functor = K.function([model.input], [layer.get_output_at(0)])
@@ -155,8 +167,6 @@ def get_layer_output(img, model, layer_name):
     # functor = K.function([model.input], [layer.output])
     outputs = functor([img])[0]
     return outputs
-
-
 while (True):
     # ret, img = cap.read()
     print('Load picture again!')
@@ -164,21 +174,20 @@ while (True):
     img = (img.copy() - 127.5) / 127.5
     origin_h, origin_w, ch = img.shape
     img = np.expand_dims(img, axis=0)
+    shape_img = [origin_w, origin_h]
+    shape_img = np.expand_dims(np.array(shape_img), axis=0)
     ###   -------------------   ###
     ###   scale by tensorflow   ###
     ###   -------------------   ###
-
     # tf_img_in = tf.placeholder(dtype=tf.float32, shape=(None, None, 3))
     # scale = 3
     # tf_img_op1 = tf.image.resize_images(tf_img_in, [h * scale, w * scale], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     # sess = tf.Session()
     # sess.run(tf.global_variables_initializer())
     # img_op1 = sess.run([tf_img_op1], feed_dict={tf_img_in: img})
-
     ###   -------------------   ###
     ###   scale by keras model  ###
     ###   -------------------   ###
-
     # input_img = Input(shape=[None, None, 3], name='img')
     # Scale_layer = Lambda(resize_img, name='lambda_scale')(input_img)
     # model = Model(input_img, Scale_layer)
@@ -188,7 +197,6 @@ while (True):
     # image_height = get_layer_output(img, model, 'lambda_scale')
     # print('Done')
     # for rectangle in rectangles:
-    #
     #     if rectangle is not None:
     #         W = -int(rectangle[0]) + int(rectangle[2])
     #         H = -int(rectangle[1]) + int(rectangle[3])
@@ -201,7 +209,6 @@ while (True):
     #         if crop_img.shape[0] < 0 or crop_img.shape[1] < 0:
     #             continue
     #         cv2.rectangle(draw, (int(rectangle[0]), int(rectangle[1])), (int(rectangle[2]), int(rectangle[3])), (255, 0, 0), 1)
-    #
     #         for i in range(5, 15, 2):
     #             cv2.circle(draw, (int(rectangle[i + 0]), int(rectangle[i + 1])), 2, (0, 255, 0))
     # cv2.imshow("test", draw)
@@ -209,10 +216,9 @@ while (True):
     # print(c)
     # if c == 27 or c == ord('q'):
     #     break
-
     # cv2.imwrite('test.jpg', draw)
-
-    output = Pnet.predict(img)
+    # pnet_layer_output = get_layer_output(img, Pnet, 'Pnet_output_post')
+    output = Pnet.predict([img, shape_img])
     # cls_pro, roi = Pnet_out(ouput)
     # model = Model(ouput, pro_bbox)
     # image_scaled = model.predict(img)
