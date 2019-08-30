c5c74d42db508f2f298260fb541fdbaffe13c46f
Hamiltonsjtu <zhushuaisjtu@qq.com>
2019-08-30 11:27:18 +0800

    TrainTripletLossOwnDataset

--- triplet-loss-tf/TrainTripletOwnData.py
+++ triplet-loss-tf/TrainTripletOwnData.py
@@ -2,8 +2,9 @@
 
 import argparse
 import os
-
+import cv2
 import tensorflow as tf
+import numpy as np
 
 from model.input_fn import train_input_fn
 from model.input_fn import test_input_fn
@@ -15,10 +16,31 @@ parser = argparse.ArgumentParser()
 # parser.add_argument('--model_dir', default='experiments/base_model',
 parser.add_argument('--model_dir', default='experiments/batch_all',
                     help="Experiment directory containing params.json")
-parser.add_argument('--data_dir', default=r'E:\shuai\Face\triplet-loss-tf',
+parser.add_argument('--data_dir', default=r'F:\110105036-2126-5505_OK_fill',
                     help="Directory containing the dataset")
+BATCH_SIZE = 64
+EPOCHS = 5
+
+def loadImageLabel(data_dir):
+    images = []
+    labels = []
+    for i in os.listdir(data_dir):
+        for j in os.listdir(data_dir + '/' + i):
+            images.append(cv2.imread(data_dir + '/' + i + '/' + j))
+            labels.append(i)
+
+    return np.asarray(images), np.asarray(labels)
 
 
+def input_fn(images, labels, epochs, batch_size):
+    # Convert the inputs to a Dataset. (E)
+    dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+    # Shuffle, repeat, and batch the examples. (T)
+    SHUFFLE_SIZE = 5000
+    dataset = dataset.shuffle(SHUFFLE_SIZE).repeat(epochs).batch(batch_size)
+    dataset = dataset.prefetch(None)
+    # Return the dataset. (L)
+    return dataset
 
 
 if __name__ == '__main__':
@@ -41,9 +63,9 @@ if __name__ == '__main__':
     # Train the model
     tf.logging.info("Starting training for {} epoch(s).".format(params.num_epochs))
     # 'Train dateset'
-    train_dataset = train_input_fn(args.data_dir, params)
+    train_images, train_labels = loadImageLabel(args.data_dir)
 
-    estimator.train(lambda: train_input_fn(args.data_dir, params))
+    estimator.train(lambda: input_fn(train_images, train_labels, epochs=EPOCHS, batch_size=BATCH_SIZE))
 
     # Evaluate the model on the test set
     tf.logging.info("Evaluation on test set.")
